af9d9e8f8e21336e919dc06821f247a7b79c1050
cjheyde <carlaheyde@gmail.com>
2023-11-29 18:29:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Footer';
+import Form from './Form';
+import OtherComponent from './OtherComponent';
+// import Header from './Header';
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Form />
+      <Footer />
+      <OtherComponent />
+    </>
+  );
+}
+
+export default App;
