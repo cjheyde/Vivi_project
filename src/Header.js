495c457039f77dd4be18af49dacbe9458709e872
cjheyde <carlaheyde@gmail.com>
2023-11-29 18:51:31 +0100

    feat: :construction: Shows content from App.js in NewComponent

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+// Header.js
+import React from 'react';
+
+const Header = () => {
+    return (
+    <nav id="topnav">
+        {/* Navbar links */}
+        <div className="nav-links">
+        <ul className="navbar-links">
+            {/* Link "Shop" */}
+            <li className="nav-item">
+                Header
+            </li>
+        </ul>
+        </div>
+    </nav>
+    );
+};
+
+export default Header;
